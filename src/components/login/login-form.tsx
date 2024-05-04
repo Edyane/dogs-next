197a20a6b4a62a303dae924790f581b619005b14
Edyane <edyanealves@gmail.com>
2024-05-04 15:52:31 -0300

    feat: adicionando mensagem de erro e redirecionando para conta

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -4,38 +4,45 @@ import login from '@/actions/login';
 import { useFormState, useFormStatus } from 'react-dom';
 import Button from '@/components/forms/button';
 import Input from '@/components/forms/input';
+import ErrorMessage from '@/components/helper/error-message';
+import React, {useEffect} from 'react';
+import Link from 'next/link';
+import styles from './login-form.module.css';
 
 function FormButton() {
-  const { pending } = useFormStatus();
+	const { pending } = useFormStatus();
 
-  return (
-    <>
-      {pending ? (
-        <Button disabled={pending}>Enviando...</Button>
-      ) : (
-        <Button>Entrar</Button>
-      )}
-    </>
-  );
+	return (
+		<>
+			{pending ? (
+				<Button disabled={pending}>Enviando...</Button>
+			) : (
+				<Button>Entrar</Button>
+			)}
+		</>
+	);
 }
 
 export default function LoginForm() {
-  const [state, action] = useFormState(login, {
-    ok: false,
-    error: '',
-    data: null,
-  });
+	const [state, action] = useFormState(login, {
+		ok: false,
+		error: '',
+		data: null,
+	});
 
-  return (
-    <>
-      <form action={action}>
-        <Input label="Usuário" />
-        <Input label="Senha" />
-        <input type="text" name="username" placeholder="usuário" />
-        <input type="password" name="password" placeholder="senha" />
-        <FormButton />
-        <p>{state.error}</p>
-      </form>
-    </>
-  );
+	useEffect(() => {
+		if (state.ok) window.location.href = '/conta';
+	}, [state.ok])
+
+	return (
+		<>
+			<form action={action} className={styles.form}>
+				<Input label="Usuário" name="username" type='text' />
+				<Input label="Senha" name="password" type='password' />
+				<ErrorMessage error={state.error} />
+				<FormButton />
+			</form>
+			<Link className={styles.perdeu} href="/login/perdeu">Perdeu a senha?</Link>
+		</>
+	);
 }
