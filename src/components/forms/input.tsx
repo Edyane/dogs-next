d0c3ef8d9e68c94b7a53812f3b189e8b9db718ac
Edyane <edyanealves@gmail.com>
2024-05-07 22:02:57 -0300

    feat: tipando os inputs

--- src/components/forms/input.tsx
+++ src/components/forms/input.tsx
@@ -1,11 +1,17 @@
-type InputProps = {
+import styles from './input.module.css';
+
+type InputProps = React.ComponentProps<'input'> & {
     label: string;
+    error?: string;
 }
-export default function Input({ label }: InputProps) {
+export default function Input({ label, error, ...props }: InputProps) {
     return (
-        <div>
-            <label htmlFor=""></label>
-            <input type="text" />
+        <div className={styles.wrapper}>
+            <label className={styles.label} htmlFor={props.name}>{label}</label>
+            <input className={styles.input} type="text" id={props.name} {...props} />
+            {error &&
+                <p className={styles.error}>{error}</p>
+            }
         </div>
     );
 }
