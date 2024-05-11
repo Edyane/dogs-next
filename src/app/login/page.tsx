e5f0cf9f8673ce748d3b7f05e4d49087c1802b44
Edyane <edyanealves@gmail.com>
2024-05-11 17:17:06 -0300

    feat: adicionando metadata na página de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,17 @@
 import LoginForm from "@/components/login/login-form";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Login | Dogs',
+    description: 'Faça login na sua conta no site Dogs.',
+};
+
 export default async function LoginPage() {
     return (
         <section className="amimeLeft">
             <h1 className="title">Login</h1>
             <LoginForm />
         </section>
-    )
+    );
 }
