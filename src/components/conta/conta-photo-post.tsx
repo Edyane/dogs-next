7d1076b2d040d943e8bd22e84b29ce596ab418e3
Edyane <edyanealves@gmail.com>
2024-06-22 22:51:41 -0300

    feat adicionando try catch

--- src/components/conta/conta-photo-post.tsx
+++ src/components/conta/conta-photo-post.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import React from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 
 export default function ContaPhotoPost() {
+	// const [state, action] = useFormState(photoPost, {
+	// 	ok: false,
+	// 	error: '',
+	// 	data: null,
+	// });
+
 	return (
 		<h1>photo post</h1>
 	);
