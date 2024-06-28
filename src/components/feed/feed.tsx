e8a9f4d18bc4db7f33d22c16826f232f6c88429e
Edyane <edyanealves@gmail.com>
2024-06-27 23:30:01 -0300

    feat: construindo scroll infinito

--- src/components/feed/feed.tsx
+++ src/components/feed/feed.tsx
@@ -1,7 +1,11 @@
+import React, { useState } from 'react';
 import { Photo } from '@/actions/photos-get';
 import FeedPhotos from "./feed-photos";
 
 export default function Feed({ photos } : { photos: Photo[] }) {
+    const [modalPhoto, setModalPhoto] = useState(null);
+    const [pages, setPages] = useState([1]);
+    const [infinite, setInfinite] = useState(true)
 
     return (
         <div>
