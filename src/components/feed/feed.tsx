d3d4a3226238db34249107ffc389ba4196064462
Edyane <edyanealves@gmail.com>
2024-06-28 22:08:42 -0300

    feat: importando feedModal

--- src/components/feed/feed.tsx
+++ src/components/feed/feed.tsx
@@ -2,10 +2,34 @@ import React, { useState } from 'react';
 import { Photo } from '@/actions/photos-get';
 import FeedPhotos from "./feed-photos";
 
-export default function Feed({ photos } : { photos: Photo[] }) {
+export default function Feed({ photos }: { photos: Photo[] }) {
     const [modalPhoto, setModalPhoto] = useState(null);
     const [pages, setPages] = useState([1]);
-    const [infinite, setInfinite] = useState(true)
+    const [infinite, setInfinite] = useState(true);
+
+    useEffect(() => {
+        let wait = false;
+        function infiniteScroll() {
+            if (infinite) {
+                const scroll = window.scrollY;
+                const height = document.body.offsetHeight - window.innerHeight;
+                if (scroll > height * 0.75 && !wait) {
+                    setPages((pages) => [...pages, pages.length + 1]);
+                    wait = true;
+                    setTimeout(() => {
+                        wait = false;
+                    }, 500);
+                }
+            }
+        }
+
+        window.addEventListener('wheel', infiniteScroll);
+        window.addEventListener('scroll', infiniteScroll);
+        return () => {
+            window.removeEventListener('wheel', infiniteScroll);
+            window.removeEventListener('scroll', infiniteScroll);
+        };
+    }, [infinite]);
 
     return (
         <div>
