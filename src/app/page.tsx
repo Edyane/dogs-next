23088cacd8d745696f3be4b5d418bc2b892920f7
Edyane <edyanealves@gmail.com>
2024-07-25 22:38:26 -0300

    refactor: ajustando a home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import photosGet from '@/actions/photos-get';
 import Feed from '@/components/feed/feed';
 
 export default async function Home() {
-	const data = await photosGet();
-
-	return (
-		<section className="container mainContainer">
-			<h1 className="title">Dogs</h1>
-			<Feed photos={data} />
-		</section>
-	);
+  const { data } = await photosGet();
+  return (
+    <section className="container mainContainer">
+      {data && <Feed photos={data} />}
+    </section>
+  );
 }
