c44214c905aac9db91648e9315f9a9ebae2556c5
Edyane <edyanealves@gmail.com>
2024-07-31 22:31:44 -0300

    feat: adicionando use-media

--- src/hooks/use-media.ts
+++ src/hooks/use-media.ts
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const useMedia = (media: string) => {
-	const [match, setMatch] = React.useState<boolean>(false);
+  const [match, setMatch] = React.useState<boolean>(false);
 
-	React.useEffect(() => {
-		function changeMatch() {
-			const { matches } = window.matchMedia(media);
-			setMatch(matches);
-		}
-		changeMatch();
-		window.addEventListener("resize", changeMatch);
-		return () => {
-			window.removeEventListener("resize", changeMatch);
-		};
-	}, [media]);
+  React.useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener('resize', changeMatch);
+    return () => {
+      window.removeEventListener('resize', changeMatch);
+    };
+  }, [media]);
 
-	return match;
+  return match;
 };
 
 export default useMedia;
