596772f12eec3da52576b23b291c05b2a241382d
Edyane <edyanealves@gmail.com>
2024-08-04 18:54:34 -0300

    feat: adicionando página de perdeu a senha

--- src/app/login/perdeu/page.tsx
+++ src/app/login/perdeu/page.tsx
@@ -2,15 +2,15 @@ import LoginPerdeuForm from '@/components/login/login-perdeu-form';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Perdeu a senha | Dogs',
-    description: 'Recupere a sua senha',
+  title: 'Perdeu a senha | Dogs',
+  description: 'Recupere a sua senha',
 };
 
 export default async function PerdeuPage() {
-    return (
-        <div className="animeLeft">
-            <h1 className="title">Perdeu a senha?</h1>
-            <LoginPerdeuForm />
-        </div>
-    );
+  return (
+    <div className="animeLeft">
+      <h1 className="title">Perdeu a senha?</h1>
+      <LoginPerdeuForm />
+    </div>
+  );
 }
